8517d308dbf294d126da8e1d25616088ab7a7f7c
Ali Narangifard <alinar@AliNMP.scilifelab.se>
2018-08-31 18:47:17 +0200

    element detection fixed

--- molar/atom.py
+++ molar/atom.py
@@ -4,17 +4,17 @@ import atomic_mass
 class Atom:
     
     # Static variables #
-    one_letter_elements = {"H","B","C","N","O","F","P","K","S","V","I","U"}     # one letter elements
-    two_letter_elements = {" H","He","Li","Be"," B"," C"," N"," O"," F","Ne",   # one and two letter elements
-                           "Na","Mg","Al","Si"," P"," S","Cl","Ar"," K","Ca",   # the 2nd letter is in the lower-case.
-                           "Sc","Ti"," V","Cr","Mn","Fe","Co","Ni","Cu","Zn",
-                           "Ga","Ge","As","Se","Br","Kr","Rb","Sr"," Y","Zr",
+    one_letter_elements = {"H","B","C","N","O","F","P","K","S","V","I","U","Y","W","I"}     # one letter elements
+    two_letter_elements = {"He","Li","Be","Ne",   
+                           "Na","Mg","Al","Si","Cl","Ar","Ca",   # the 2nd letter is in the lower-case.
+                           "Sc","Ti","Cr","Mn","Fe","Co","Ni","Cu","Zn",
+                           "Ga","Ge","As","Se","Br","Kr","Rb","Sr","Zr",
                            "Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn",
-                           "Sb","Te"," I","Xe","Cs","Ba","La","Ce","Pr","Nd",
+                           "Sb","Te","Xe","Cs","Ba","La","Ce","Pr","Nd",
                            "Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb",
-                           "Lu","Hf","Ta"," W","Re","Os","Ir","Pt","Au","Hg",
+                           "Lu","Hf","Ta","Re","Os","Ir","Pt","Au","Hg",
                            "Tl","Pb","Bi","Po","At","Rn","Fr","Ra","Ac","Th",
-                           "Pa"," U","Np","Pu","Am","Cm","Bk","Cf"} #
+                           "Pa","Np","Pu","Am","Cm","Bk","Cf"} #
     def __init__(self,line_ext):
         self.pos = np.array([0,0,0],dtype='f')
         line_ext=line_ext.replace("\n","") # getting rid of \n and \r in the line
@@ -24,9 +24,9 @@ class Atom:
         self.pos[1] = float(self.line[38:46])
         self.pos[2] = float(self.line[46:54])
         self.name   = self.line[12:16]
+        self.resname = self.line[17:21]
         self.index  = 1
-        ## Element ##
-        self.element = self.line[76:78]
+        self.element = self.ExtractElement()
         self.bonded_atoms=[]
         
     def ApplyTransform(self,trans): # trans is a vtk.vtkTransfom()
@@ -54,7 +54,7 @@ class Atom:
         self.UpdateCrd()
         self.line  = self.line[:6]   + '%5d' % (atom_sq_number % 100000)  + self.line[11:]
         self.line  = self.line[:22] + '%4d' % (res_sq_number % 10000)    + self.line[26:]
-        self.line  = self.line[:76] + '%2s' % self.ExtractElement()[-2:] + self.line[78:]
+        self.line  = self.line[:76] + '%2s' % self.element()[-2:] + self.line[78:]
         self.index = atom_index
         return self.line
     
@@ -66,17 +66,16 @@ class Atom:
     def ExtractElement(self):
         """ Try to extract the atom element from the atom's name.
         """
-        if self.element not in {""," ","  "}:
-            return self.element
-        for i in range(4):
-            if self.name[i] == " ":   # getting rid of spaces on the right side.
-                continue
-            else:
-                if self.name[i:i+2] in Atom.two_letter_elements: # two letter elements
-                    return self.name[i:i+2].upper()              # return element in upper case.
-                else:
-                    if self.name[i] in Atom.one_letter_elements: # one letter elements
-                        return self.name[i].upper()              # return element in upper case.
+        element_str = self.line[76:78].strip()
+        name_strip = self.name.strip()
+        if element_str !='' :
+            return element_str
+        if name_strip[0:2] in Atom.two_letter_elements: # two letter elements
+            out = name_strip[0].upper() + name_strip[1].lower()              # return element in upper case.
+            return out.strip()
+        else:
+            if name_strip[0] in Atom.one_letter_elements: # one letter elements
+                return name_strip[0].upper()              # return element in upper case.
         return ""
                     
                 
