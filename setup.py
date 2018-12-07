aedc37b6d872557eaa5de3e2ec7c8e4ddf1e480f
Ali Narangifard <alinar@AliNMP.scilifelab.se>
2018-12-07 10:56:08 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 from setuptools import setup, find_packages
 
 setup(name='molar',
-      version='2.1.0',
+      version='2.1.1',
       description='',
       url='https://github.com/alinar/Molar',
       author='Ali Narangifard',
